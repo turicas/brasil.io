1c00c03ef1556f2578525134f69f5359f8ef4c27
Raphael Amorim <rapha850@gmail.com>
2014-05-14 18:43:19 -0300

    others little changes

--- _site/assets/javascript/main.js
+++ _site/assets/javascript/main.js
@@ -1,10 +1,8 @@
 var url = window.location.pathname,
     urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/, ''));
 
-$('#pagenav a')
-    .each(function() {
-        if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
-            $(this).find('li').addClass('active');
-        }
-    });
+$('#pagenav a').each(function() {
+    if (urlRegExp.test(this.href.replace(/\/$/, '')))
+        $(this).find('li').addClass('active');
+});
 
