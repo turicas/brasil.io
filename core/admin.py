7db29e5410ae87e9d0f558abf999cf13550c9842
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2018-03-26 03:44:10 -0300

    Add core models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from core import models
+
+
+class DatasetAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Dataset, DatasetAdmin)
+
+
+class LinkAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Link, LinkAdmin)
+
+
+class VersionAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('dataset')
+admin.site.register(models.Version, VersionAdmin)
+
+
+class TableAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        return super().get_queryset(request)\
+                      .select_related('dataset', 'version')
+admin.site.register(models.Table, TableAdmin)
+
+
+class FieldAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        return super().get_queryset(request)\
+                      .select_related('dataset', 'version', 'table')
+admin.site.register(models.Field, FieldAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,83 @@
-from django.db import models
+from urllib.parse import urlparse
 
-# Create your models here.
+from django.db import models
+from django.contrib.postgres.fields import ArrayField, JSONField
+
+
+class Dataset(models.Model):
+    author_name = models.CharField(max_length=255, null=False, blank=False)
+    author_url = models.URLField(max_length=2000, null=True, blank=True)
+    code_url = models.URLField(max_length=2000, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    license_name = models.CharField(max_length=255, null=False, blank=False)
+    license_url = models.URLField(max_length=2000, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    slug = models.SlugField(max_length=50, null=False, blank=False)
+    source_name = models.CharField(max_length=255, null=False, blank=False)
+    source_url = models.URLField(max_length=2000, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name} (by {self.author_name}, source: {self.source_name})'
+
+
+class Link(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE,
+                                null=False, blank=False)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    url = models.URLField(max_length=2000, null=False, blank=False)
+
+    def __str__(self):
+        domain = urlparse(self.url).netloc
+        return f'{self.title} ({domain})'
+
+
+class Version(models.Model):
+    collected_at = models.DateField(null=False, blank=False)
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE,
+                                null=False, blank=False)
+    download_url = models.URLField(max_length=2000, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    order = models.PositiveIntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.dataset.slug}.{self.name} (order: {self.order})'
+
+
+class Table(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE,
+                                null=False, blank=False)
+    default = models.BooleanField(null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    options = JSONField(null=True, blank=True)
+    ordering = ArrayField(models.CharField(max_length=63),
+                          null=False, blank=False)
+    version = models.ForeignKey(Version, on_delete=models.CASCADE,
+                                null=False, blank=False)
+
+
+    def __str__(self):
+        return f'{self.dataset.slug}.{self.version.name}.{self.name}'
+
+
+class Field(models.Model):
+    TYPES = ['binary', 'bool', 'date', 'datetime', 'decimal', 'email',
+             'float', 'integer', 'json', 'percent', 'text',]
+    TYPE_CHOICES = [(value, value) for value in TYPES]
+
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE,
+                                null=False, blank=False)
+    index = models.BooleanField(null=False, blank=False)
+    max_length = models.PositiveSmallIntegerField(null=True, blank=True)
+    name = models.CharField(max_length=63)
+    options = JSONField(null=True, blank=True)
+    show = models.BooleanField(null=False, blank=False)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE,
+                              null=False, blank=False)
+    type = models.CharField(max_length=15, choices=TYPE_CHOICES,
+                            null=False, blank=False)
+    version = models.ForeignKey(Version, on_delete=models.CASCADE,
+                                null=True, blank=True)
+
+    def __str__(self):
+        return (f'{self.dataset.slug}.{self.version.name}.{self.table.name}'
+                f'.{self.name}')
