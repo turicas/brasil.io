7f2da9a2215e0c4ad52b5d39b18410fc8a4beabb
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2018-04-16 19:56:39 -0300

    Create base template and all pages

--- core/views.py
+++ core/views.py
@@ -7,14 +7,14 @@ from core.models import Dataset
 def contact(request):
     return render(request, 'contact.html', {})
 
-def dataset_list(request):
-    context = {'datasets': Dataset.objects.all()}
-    return render(request, 'dataset-list.html', context)
-
 def dataset_detail(request, slug):
     dataset = get_object_or_404(Dataset, slug=slug)
     context = {'dataset': dataset}
     return render(request, 'dataset-detail.html', context)
+
+def dataset_list(request):
+    context = {'datasets': Dataset.objects.all()}
+    return render(request, 'dataset-list.html', context)
 
 def dataset_suggestion(request):
     return render(request, 'dataset-suggestion.html', {})
