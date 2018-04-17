4f6602a667ce7e7c285337fdf79cce6887a812b0
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2018-04-17 09:16:50 -0300

    Add first special collection: companies

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 
-from . import views
+from . import views, views_special
 
 
 app_name = 'core'
 urlpatterns = [
+    # Institutional pages
     path('', views.index, name='index'),
     path('contato', views.contact, name='contact'),
     path('datasets', views.dataset_list, name='dataset-list'),
     path('dataset/<slug>', views.dataset_detail, name='dataset-detail'),
     path('datasets/sugira', views.dataset_suggestion, name='dataset-suggestion'),
     path('manifesto', views.manifesto, name='manifesto'),
+
+    # Dataset-specific pages (specials)
+    path('especiais/empresas', views_special.companies, name='special-companies'),
+    path('especiais/empresa/<document>', views_special.company_detail, name='special-company-detail'),
 ]
---
+++ core/views_special.py
@@ -0,0 +1,50 @@
+from django.contrib.postgres.search import SearchQuery
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+
+from core.models import Dataset
+
+
+def companies(request):
+    Socios = Dataset.objects.get(slug='socios-brasil').get_last_data_model()
+    result, total, max_results = [], 0, 500
+    search_query = request.GET.get('q')
+
+    if search_query:
+        result = Socios.objects.filter(search_data=SearchQuery(search_query))\
+                               .values_list('cnpj_empresa', 'nome_empresa')\
+                               .distinct()\
+                               .order_by('nome_empresa')
+        total = result.count()
+        result = result[:max_results]
+
+    context = {
+        'result': result,
+        'max_results': max_results,
+        'search_query': search_query,
+        'total': total,
+    }
+    return render(request, 'specials/companies.html', context)
+
+def company_detail(request, document):
+    document = document.replace('.', '').replace('-', '').replace('/', '').strip()
+    Socios = Dataset.objects.get(slug='socios-brasil').get_last_data_model()
+    Gastos = Dataset.objects.get(slug='gastos-deputados').get_last_data_model()
+
+    partners = Socios.objects.filter(cnpj_empresa=document)\
+                             .order_by('nome_socio')
+    first_partner = partners.first()
+    spending = Gastos.objects.filter(txtcnpjcpf=document)\
+                             .order_by('-datemissao')
+    company = {
+            'document': document,
+            'name': first_partner.nome_empresa,
+            'state': first_partner.unidade_federativa,
+    }
+
+    context = {
+        'company': company,
+        'partners': partners,
+        'spending': spending,
+    }
+    return render(request, 'specials/company-detail.html', context)
