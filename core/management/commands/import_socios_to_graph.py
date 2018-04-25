f0fa12e50e4ae78c9522e9e37cfde34e97ae6003
Bernardo Fontes <bernardoxhc@gmail.com>
2018-04-25 19:48:07 -0300

    Import dados dos sócios também

--- core/management/commands/import_socios_to_graph.py
+++ core/management/commands/import_socios_to_graph.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 
 from core.models import Table
 
-from graphs.nodes import PessoaJuridica
+from graphs.nodes import PessoaJuridica, PessoaFisica, NomeExterior
 from graphs.connection import graph_db
 
 
@@ -20,8 +20,60 @@ class Command(BaseCommand):
             company = PessoaJuridica()
             company.nome = partnership.nome_empresa.upper()
             company.cnpj = partnership.cnpj_empresa.upper()
+            company.uf = partnership.unidade_federativa.upper()
             graph_db.push(company)
+        elif not company.uf and partnership.unidade_federativa:
+            company.uf = partnership.unidade_federativa.upper()
+            graph_db.push(company)
+
         return company
+
+    def get_company_partner(self, partnership):
+        company = PessoaJuridica.select(graph_db, partnership.cpf_cnpj_socio).first()
+
+        if not company:
+            company = PessoaJuridica()
+            company.nome = partnership.nome_socio.upper()
+            company.cnpj = partnership.cpf_cnpj_socio.upper()
+            graph_db.push(company)
+
+        return company
+
+    def get_person_partner(self, partnership):
+        person = PessoaFisica.select(graph_db, partnership.nome_socio).first()
+
+        if not person:
+            person = PessoaFisica()
+            person.nome = partnership.nome_socio.upper()
+            person.cpf = (partnership.cpf_cnpj_socio or '').upper()
+            graph_db.push(person)
+
+        return person
+
+    def get_foreigner_partner(self, partnership):
+        partner = NomeExterior.select(graph_db, partnership.nome_socio).first()
+
+        if not partner:
+            partner = NomeExterior()
+            partner.nome = partnership.nome_socio.upper()
+            partner.cpf_cnpj = (partnership.cpf_cnpj_socio or '').upper()
+            graph_db.push(partner)
+
+        return partner
+
+    def get_partner(self, partnership):
+        partner_type = partnership.codigo_tipo_socio
+        if partner_type == 1:
+            return self.get_company_partner(partnership)
+        elif partner_type == 2:
+            return self.get_person_partner(partnership)
+        elif partner_type == 3:
+            return self.get_foreigner_partner(partnership)
+        else:
+            msg = 'Sódio {} - {} é um tipo inválido.'.format(
+                partnership.codigo_tipo_socio, partnership.tipo_socio
+            )
+            raise ValueError(msg)
 
     def handle(self, *args, **kwargs):
         SociosBrasil = self.get_socios_brasil_model()
@@ -30,6 +82,7 @@ class Command(BaseCommand):
         start = time.time()
         for partnership in SociosBrasil.objects.all()[:100].iterator():
             company = self.get_company(partnership)
+            partner = self.get_partner(partnership)
 
         end = time.time()
         print('  finalizado em {:.3f}s.'.format(end - start))
--- graphs/nodes.py
+++ graphs/nodes.py
@@ -1,9 +1,23 @@
 from py2neo.ogm import GraphObject, Property
 
 
+class PessoaFisica(GraphObject):
+    __primarykey__ = "nome"
+
+    nome = Property()
+    cpf = Property()
+
+
 class PessoaJuridica(GraphObject):
     __primarykey__ = "cnpj"
 
     nome = Property()
     cnpj = Property()
     uf = Property()
+
+
+class NomeExterior(GraphObject):
+    __primarykey__ = "nome"
+
+    nome = Property()
+    cpf_cnpj = Property()
