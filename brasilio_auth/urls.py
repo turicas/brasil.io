9eac3d0bb181543be2c4325c111cd7d160e88e97
Bernardo Fontes <bernardoxhc@gmail.com>
2018-05-11 17:16:08 -0300

    Adiciona view de logout

--- brasilio_auth/urls.py
+++ brasilio_auth/urls.py
@@ -4,9 +4,11 @@ from django.urls import path
 
 views_config = {
     'login': auth_views.LoginView.as_view(template_name='brasilio_auth/login.html'),
+    'logout': auth_views.LogoutView.as_view(),
 }
 
 app_name = 'brasilio_auth'
 urlpatterns = (
     path('login/', views_config['login'], name='login'),
+    path('logout/', views_config['logout'], name='logout'),
 )
