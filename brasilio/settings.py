beb60b366db968f38e102a2fdad8aca48bda53d3
Bernardo Fontes <bernardoxhc@gmail.com>
2018-05-11 17:00:38 -0300

    Add app skeleton to address any authentication customization

--- brasilio/settings.py
+++ brasilio/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
     # Project apps
     'core',
+    'brasilio_auth',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ brasilio_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BrasilioAuthConfig(AppConfig):
+    name = 'brasilio_auth'
