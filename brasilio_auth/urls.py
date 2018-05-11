4c1d0f300f9027448cc1096911ad09df906d5d0c
Bernardo Fontes <bernardoxhc@gmail.com>
2018-05-11 17:42:52 -0300

    Implementa a primeira interação do fluxo de resetar senha

--- brasilio_auth/urls.py
+++ brasilio_auth/urls.py
@@ -5,10 +5,19 @@ from django.urls import path
 views_config = {
     'login': auth_views.LoginView.as_view(template_name='brasilio_auth/login.html'),
     'logout': auth_views.LogoutView.as_view(),
+    'password_reset': auth_views.PasswordResetView.as_view(
+        template_name='brasilio_auth/password_reset.html',
+        success_url='/auth/troca-senha/enviada/',
+    ),
+    'password_reset_done': auth_views.PasswordResetDoneView.as_view(
+        template_name='brasilio_auth/password_reset_done.html',
+    ),
 }
 
 app_name = 'brasilio_auth'
 urlpatterns = (
     path('login/', views_config['login'], name='login'),
     path('logout/', views_config['logout'], name='logout'),
+    path('troca-senha/', views_config['password_reset'], name='password_reset'),
+    path('troca-senha/enviada/', views_config['password_reset_done'], name='password_reset_done'),
 )
