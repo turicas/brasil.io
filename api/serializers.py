05a3b8064bed8e2a57cbd915b94976a1b4c5551b
Eduardo Stalinho <eduardooc.86@gmail.com>
2018-05-31 16:41:35 -0300

    Add schema field in dataset serializer using get_table method.

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
-from core.models import Dataset, Link
+from core.models import Dataset, Field, Link, Table
 
 
 class LinkSerializer(serializers.ModelSerializer):
@@ -9,6 +9,21 @@ class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
         fields = ('title', 'url')
+
+
+class FieldSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Field
+        fields = ('name', 'type')
+
+
+class TableSerializer(serializers.ModelSerializer):
+    fields = FieldSerializer(many=True, source='field_set')
+
+    class Meta:
+        model = Table
+        fields = ('fields',)
 
 
 class DatasetSerializer(serializers.ModelSerializer):
@@ -31,7 +46,7 @@ class DatasetDetailSerializer(serializers.ModelSerializer):
     data_url = serializers.SerializerMethodField()
     id = serializers.SerializerMethodField()
     links = LinkSerializer(many=True, source='link_set')
-    # TODO: add schema
+    schema = TableSerializer(source='get_table')
 
     def get_id(self, obj):
         return reverse('api:dataset-detail', kwargs={'slug': obj.slug},
@@ -46,8 +61,9 @@ class DatasetDetailSerializer(serializers.ModelSerializer):
         fields = (
             'author_name', 'author_url', 'code_url', 'data_url', 'description',
             'id', 'license_name', 'license_url', 'links', 'name', 'slug',
-            'source_name', 'source_url',
+            'source_name', 'source_url', 'schema'
         )
+
 
 class GenericSerializer(serializers.ModelSerializer):
 
