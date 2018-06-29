cf26b84e69ead9e4e71df70eb5b6ca3367a73238
Bernardo Fontes <bernardoxhc@gmail.com>
2018-06-29 16:36:22 -0300

    Refator form para traçar rota entre empresas para recuperar CNPJ do banco de documentos

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.core.exceptions import ObjectDoesNotExist
 
 from core.models import Dataset
+from core.util import get_company_by_document
 
 
 def _resolve_field_by_type(person_type):
@@ -10,18 +12,24 @@ def _resolve_field_by_type(person_type):
         return 'cnpj'
 
 
-def _get_obj(field, identifier):
-    Socios = Dataset.objects.get(slug='socios-brasil')\
-                            .get_table('socios')\
-                            .get_model()
-    return Socios.objects.filter(**{field: identifier}).first()
+def _get_obj(field, identifier, person_type):
+    if person_type == 'pessoa-fisica':
+        Socios = Dataset.objects.get(slug='socios-brasil')\
+                                .get_table('socios')\
+                                .get_model()
+        return Socios.objects.filter(**{field: identifier}).first()
+    elif person_type == 'pessoa-juridica':
+        try:
+            return get_company_by_document(identifier)
+        except ObjectDoesNotExist:
+            return None
 
 
 def _get_name(obj, person_type):
     if person_type == 'pessoa-fisica':
         return obj.nome_socio
     elif person_type == 'pessoa-juridica':
-        return obj.razao_social
+        return obj.name
 
 
 class TracePathForm(forms.Form):
@@ -43,11 +51,13 @@ class TracePathForm(forms.Form):
         destination_type = cleaned_data.get('destination_type')
         destination_identifier = cleaned_data.get('destination_identifier')
 
+
         origin_field = _resolve_field_by_type(origin_type)
         destination_field = _resolve_field_by_type(destination_type)
-        origin = _get_obj(origin_field, origin_identifier)
-        destination = _get_obj(destination_field, destination_identifier)
+        origin = _get_obj(origin_field, origin_identifier, origin_type)
+        destination = _get_obj(destination_field, destination_identifier, destination_type)
 
+        import ipdb; ipdb.set_trace()
         if origin is None:
             self.add_error('origin_identifier', 'Name/document not found')
         else:
