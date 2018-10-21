9576741c67691a944d06ac45ef36643248a7ac3e
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2018-10-21 00:55:48 -0300

    PEP-8 on changed files

--- core/util.py
+++ core/util.py
@@ -23,21 +23,22 @@ def create_object(Model, data):
     )
 
     for field in Model._meta.fields:
-        if (isinstance(field, special_fields) and
-            not (data.get(field.name) or '').strip()):
+        if (
+            isinstance(field, special_fields)
+            and not (data.get(field.name) or "").strip()
+        ):
             data[field.name] = None
 
     return Model(**data)
 
 
 def get_company_by_document(document):
-    Documents = Table.objects.for_dataset('documentos-brasil').named('documents').get_model()
-    doc_prefix = document[:8]
-    headquarter_prefix = doc_prefix + '0001'
-    branches = Documents.objects.filter(
-        docroot=doc_prefix,
-        document_type='CNPJ',
+    Documents = (
+        Table.objects.for_dataset("documentos-brasil").named("documents").get_model()
     )
+    doc_prefix = document[:8]
+    headquarter_prefix = doc_prefix + "0001"
+    branches = Documents.objects.filter(docroot=doc_prefix, document_type="CNPJ")
     if not branches.exists():
         # no document found with this prefix - we don't know this company
         raise Documents.DoesNotExist()
