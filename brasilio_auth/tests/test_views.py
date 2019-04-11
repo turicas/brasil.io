4bc81968e5d3323f9161d8f1d6088cd31f290ebb
Lorran Rosa <lorran@sixcreative.com.br>
2019-04-10 22:38:49 -0300

    Incluidas assertions do TestCase

--- brasilio_auth/tests/test_views.py
+++ brasilio_auth/tests/test_views.py
@@ -21,19 +21,19 @@ class UserCreationViewTests(TestCase):
     def test_render_correct_template(self):
         response = self.client.get(self.url)
         self.assertTemplateUsed(response, 'brasilio_auth/user_creation_form.html')
-        assert 'form' in response.context
+        self.assertTrue('form' in response.context)
 
     def test_render_form_errors_if_invalid_post(self):
         response = self.client.post(self.url, data={})
         self.assertTemplateUsed(response, 'brasilio_auth/user_creation_form.html')
-        assert bool(response.context['form'].errors) is True
+        self.assertTrue(bool(response.context['form'].errors))
 
     def test_create_and_login_with_user(self):
-        assert 0 == User.objects.count()
+        self.assertEquals(0, User.objects.count())
 
         response = self.client.post(self.url, data=self.data)
         user = User.objects.get(username='foo')
 
         self.assertRedirects(response, settings.LOGIN_REDIRECT_URL, fetch_redirect_response=False)
-        assert '_auth_user_id' in self.client.session
-        assert str(user.pk) == self.client.session['_auth_user_id']
+        self.assertTrue('_auth_user_id' in self.client.session)
+        self.assertEquals(str(user.pk), self.client.session['_auth_user_id'])
