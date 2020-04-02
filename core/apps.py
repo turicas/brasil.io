a94f3433dea38599988ac0daa7fb079c17bfc9d6
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-04-02 20:43:22 -0300

    Remove auto-carregamento de models dinâmicos (erro CI)
    https://github.com/turicas/brasil.io/runs/556540333

--- core/apps.py
+++ core/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = "core"
-
-    def ready(self):
-        from .models import Table
-        for table in Table.objects.all():
-            _ = table.get_model()
