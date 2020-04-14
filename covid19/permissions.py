df03dec5c993b3520640a8862d4fc71f73bd8993
Bernardo Fontes <bernardoxhc@gmail.com>
2020-04-14 16:05:03 -0300

    Context processor para sinalizar se usuária é ou não colaboradora com o dataset do covid-19

--- covid19/permissions.py
+++ covid19/permissions.py
@@ -1,6 +1,18 @@
+from localflavor.br.br_states import STATE_CHOICES
+
 from django.conf import settings
 
 
+def _format_perm_code(uf):
+    return 'covid19.' + settings.COVID_IMPORT_PERMISSION_PREFIX + uf.upper()
+
+
 def user_has_state_permission(user, uf):
-    perm_code = 'covid19.' + settings.COVID_IMPORT_PERMISSION_PREFIX + uf.upper()
-    return user.has_perm(perm_code)
+    return user.has_perm(_format_perm_code(uf))
+
+
+def user_has_covid19_permissions(user):
+    permissions = set()
+    for uf, state in STATE_CHOICES:
+        permissions.add(_format_perm_code(uf))
+    return bool(user.get_all_permissions() & permissions)
---
+++ covid19/tests/test_context_processors.py
@@ -0,0 +1,55 @@
+from model_bakery import baker
+
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser, Group
+from django.test import TestCase, RequestFactory
+
+from covid19.context_processors import is_covid19_contributor
+
+
+class TestCovid19ContributorContextProcessor(TestCase):
+
+    def setUp(self):
+        self.request = RequestFactory().get('/')
+        self.rj_group = Group.objects.get(name__endswith='Rio de Janeiro')
+
+    def test_false_if_user_is_not_logged_in(self):
+        self.request.user = AnonymousUser()
+
+        output = is_covid19_contributor(self.request)
+
+        assert {'is_covid19_contributor': False} == output
+
+    def test_false_if_user_does_not_have_permission(self):
+        self.request.user = baker.make(settings.AUTH_USER_MODEL)
+
+        output = is_covid19_contributor(self.request)
+
+        assert {'is_covid19_contributor': False} == output
+
+    def test_false_if_user_have_permission_but_is_not_staff(self):
+        user = baker.make(settings.AUTH_USER_MODEL, is_staff=False)
+        user.groups.add(self.rj_group)
+        user.refresh_from_db()
+        self.request.user = user
+
+        output = is_covid19_contributor(self.request)
+
+        assert {'is_covid19_contributor': False} == output
+
+    def test_true_for_real_contributor(self):
+        user = baker.make(settings.AUTH_USER_MODEL, is_staff=True)
+        user.groups.add(self.rj_group)
+        user.refresh_from_db()
+        self.request.user = user
+
+        output = is_covid19_contributor(self.request)
+
+        assert {'is_covid19_contributor': True} == output
+
+    def test_super_user_is_contributor(self):
+        self.request.user = baker.make(settings.AUTH_USER_MODEL, is_superuser=True)
+
+        output = is_covid19_contributor(self.request)
+
+        assert {'is_covid19_contributor': True} == output
