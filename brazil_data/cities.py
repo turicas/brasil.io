a0eddaa62d9c21664aec6b0599f3c7b89b8ae86a
Bernardo Fontes <bernardoxhc@gmail.com>
2020-04-14 22:27:45 -0300

    Refatoração para tornar código de extração de dados de cidade mais reutilizável

--- brazil_data/cities.py
+++ brazil_data/cities.py
@@ -6,24 +6,37 @@ import rows
 import rows.utils
 
 POPULATION_CSV_URL = "https://raw.githubusercontent.com/turicas/covid19-br/master/data/populacao-estimada-2019.csv"  # noqa
-POPULATION_SCHEMA_URL = "https://raw.githubusercontent.com/turicas/covid19-br/master/schema/populacao-estimada-2019.csv"  #noqa
+POPULATION_SCHEMA_URL = "https://raw.githubusercontent.com/turicas/covid19-br/master/schema/populacao-estimada-2019.csv"  # noqa
 
 
 @lru_cache(maxsize=1)
-def brazilian_cities_per_state(
-    schema_filename="schema-population.csv", population_filename="population.csv"
-):
+def extract_ibge_data(schema_filename, population_filename):
     if not Path(schema_filename).exists():
         rows.utils.download_file(POPULATION_SCHEMA_URL, filename=schema_filename)
     if not Path(population_filename).exists():
         rows.utils.download_file(POPULATION_CSV_URL, filename=population_filename)
 
     schema = rows.utils.load_schema(schema_filename)
-    table = rows.import_from_csv(
+    return rows.import_from_csv(
         population_filename, encoding="utf-8", force_types=schema
     )
-    cities = sorted(table, key=lambda row: (row.state, rows.fields.slug(row.city)))
+
+
+@lru_cache(maxsize=1)
+def ibge_data_per_state(schema_filename, population_filename):
+    data = extract_ibge_data(schema_filename, population_filename)
+    cities = sorted(data, key=lambda row: (row.state, rows.fields.slug(row.city)))
+    return {
+        state: [city for city in state_cities]
+        for state, state_cities in groupby(cities, key=lambda row: row.state)
+    }
+
+
+def brazilian_cities_per_state(
+    schema_filename="schema-population.csv", population_filename="population.csv"
+):
+    data = ibge_data_per_state(schema_filename, population_filename)
     return {
         state: [city.city for city in state_cities]
-        for state, state_cities in groupby(cities, key=lambda row: row.state)
+        for state, state_cities in data.items()
     }
