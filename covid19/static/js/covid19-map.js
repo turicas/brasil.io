a7829b3c839c3fbb200d390e636ed0d5cbaa5560
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-04-18 01:07:42 -0300

    Termina de implementar mapa

--- covid19/static/js/covid19-map.js
+++ covid19/static/js/covid19-map.js
@@ -12,19 +12,20 @@ var cityData,
     selectedPlace,
     selectedVar,
     stateGeoJSON,
-    stateLayer;
+    stateLayer,
+    varControl;
 
 colors = {
   "confirmed": "#00F",
-  "confirmed_per_100k_inhabitants": "#80F",
+  "confirmed_per_100k_inhabitants": "#2B580C",
   "deaths": "#F00",
   "death_rate_percent": "#F08",
 };
 displayText = {
   "confirmed": "Casos confirmados",
-  "confirmed_per_100k_inhabitants": "Confirmados/100k hab.",
+  "confirmed_per_100k_inhabitants": "Confirmados/100.000 hab.",
   "deaths": "Óbitos confirmados",
-  "death_rate_percent": "Letalitdade (óbitos/confirmados)",
+  "death_rate_percent": "Taxa de mortalidade",
 };
 legendBins = 5;
 countryId = 0; // Brasil
@@ -33,14 +34,15 @@ selectedVar = "confirmed";
 zeroText = {
   "confirmed": "Nenhum",
   "confirmed_per_100k_inhabitants": "Nenhum",
-  "deaths": "Nenhuma",
-  "death_rate_percent": "Nenhuma",
+  "deaths": "Nenhum",
+  "death_rate_percent": "Nenhum",
 };
 
 function getPlaceData(place) {
   return place == countryId ? countryData : cityData[place];
 }
 function opacityFromValue(value, maxValue) {
+  // TODO: should round opacity numbers to bin value in legend?
   return Math.log2(value + 1) / Math.log2(maxValue + 1);
 }
 function valueFromOpacity(opacity, maxValue) {
@@ -69,21 +71,53 @@ function stateStyle(feature) {
     weight: 0.5
   };
 }
+function changeVar(newVar) {
+  selectedVar = newVar;
+  cityLayer.setStyle(cityStyle);
+  updateLegendControl();
+}
+
+function updateLegendControl() {
+  var div = legendControl.getContainer();
+  var lastValue, displayValue;
+  var maxValue = maxValues[selectedVar];
+  var color = colors[selectedVar];
+  var zeroDisplay = zeroText[selectedVar];
+  var labels = [`<b>${displayText[selectedVar]}</b>`, ""];
+  for (var opacity = 0; opacity <= 1; opacity += 1.0 / legendBins) {
+    var value = valueFromOpacity(opacity, maxValue);
+    displayValue = lastValue === undefined ? zeroDisplay : `${lastValue} &mdash; ${value}`;
+    labels.push(`<i style="background: ${color}; opacity: ${opacity}"></i> ${displayValue}`);
+    lastValue = value + 1;
+  }
+  div.innerHTML = labels.join("<br>");
+}
 function updatePlaceDataControl(placeData) {
   var div = placeDataControl.getContainer();
   var dataLines = [];
-  Object.keys(colors).forEach(function(item) {
+  Object.keys(displayText).forEach(function(item) {
     var value = Intl.NumberFormat("pt-BR").format(placeData[item]);
     value = item.endsWith("percent") ? `${value}%` : value;
-    dataLines.push(`<dt>${displayText[item]}</dt> <dd>${value}</dd>`);
+    dataLines.push(`<dt>${displayText[item]}:</dt> <dd>${value}</dd>`);
   });
   div.innerHTML = `
     <b>${placeData.city}</b>
     <br>
     <dl>
-      ${dataLines.join("<br>")}
+      ${dataLines.join("\n")}
     </dl>
   `;
+}
+function updateVarControl() {
+  var div = varControl.getContainer();
+  var inputs = [];
+  Object.keys(displayText).forEach(function(item) {
+    inputs.push(`<label><input type="radio" class="radio-control" name="radio-var-control" value="${item}"><span>${displayText[item]}</span></label>`);
+  });
+  div.innerHTML = inputs.join("<br>");
+  jQuery(".radio-control").change(function() {
+    changeVar(jQuery(this).val());
+  });
 }
 
 function createMap() {
@@ -137,32 +171,38 @@ function updateMap() {
   }
 
   if (hasToAddLegendLayer()) {
-    legendControl = L.control({position: "bottomright"});
+    notesControl = L.control({position: "bottomleft"});
+    notesControl.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "info legend");
+      div.innerHTML = 'Fonte: Secretarias Estaduais de Saúde'
+      div.innerHTML += '<br>Coleta e análise: <a href="https://brasil.io/">Brasil.IO</a>';
+      return div;
+    };
+    notesControl.addTo(map);
+
+    legendControl = L.control({position: "bottomleft"});
     legendControl.onAdd = function (map) {
       var div = L.DomUtil.create("div", "info legend");
-      var lastValue, displayValue;
-      var maxValue = maxValues[selectedVar];
-      var color = colors[selectedVar];
-      var zeroDisplay = zeroText[selectedVar];
-      var labels = [`<b>${displayText[selectedVar]}</b>`, ""];
-      for (var opacity = 0; opacity <= 1; opacity += 1.0 / legendBins) {
-        var value = valueFromOpacity(opacity, maxValue);
-        displayValue = lastValue === undefined ? zeroDisplay : `${lastValue} &mdash; ${value}`;
-        labels.push(`<i style="background: ${color}; opacity: ${opacity}"></i> ${displayValue}`);
-        lastValue = value + 1;
-      }
-      div.innerHTML += labels.join("<br>");
       return div;
     };
     legendControl.addTo(map);
+    updateLegendControl();
 
-    placeDataControl = L.control({position: "bottomleft"});
+    placeDataControl = L.control({position: "bottomright"});
     placeDataControl.onAdd = function (map) {
       var div = L.DomUtil.create("div", "info legend");
       return div;
     };
     placeDataControl.addTo(map);
     updatePlaceDataControl(getPlaceData(selectedPlace));
+
+    varControl = L.control({position: "topright"});
+    varControl.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "info legend");
+      return div;
+    };
+    varControl.addTo(map);
+    updateVarControl();
   }
 
   if (mapHasLoaded()) {
@@ -172,6 +212,7 @@ function updateMap() {
 
 function mapLoaded() {
   jQuery("#loading").css("z-index", -999);
+  jQuery(".radio-control:first").prop("checked", true).trigger("click");
 }
 
 function retrieveData() {
