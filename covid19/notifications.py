e244a71a51533743754f513987eb7a8429be8c21
Bernardo Fontes <bernardoxhc@gmail.com>
2020-04-21 19:04:37 -0300

    Recupera dados de planilha de links por UF

--- covid19/notifications.py
+++ covid19/notifications.py
@@ -1,3 +1,24 @@
+import io
+import rows
+from functools import lru_cache
+import requests
+
+
+COLLABORATORS_SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1S77CvorwQripFZjlWTOZeBhK42rh3u57aRL1XZGhSdI/export?format=csv&id=1S77CvorwQripFZjlWTOZeBhK42rh3u57aRL1XZGhSdI&gid=0"
+
+
+@lru_cache(maxsize=1)
+def collaborators_data():
+    response = requests.get(COLLABORATORS_SPREADSHEET_URL)
+    url_table = rows.import_from_csv(io.BytesIO(response.content), encoding="utf-8")
+    return url_table
+
+
+def get_state_row(state):
+    data = collaborators_data()
+    return [r for r in data if r.uf.upper() == state.upper()][0]
+
+
 def notify_new_spreadsheet(spreadsheet):
     raise NotImplementedError
 
