8c5cdc149f3a7bc03b8d28d6b3d3e710d700d00a
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-04-25 23:51:37 -0300

    Adiciona opção de pegar GeoJSONs de maior resolução

--- covid19/geo.py
+++ covid19/geo.py
@@ -5,15 +5,17 @@ from cache_memoize import cache_memoize
 from core.util import http_get_json
 
 
-def download_city_geojson():
-    url = "https://data.brasil.io/dataset/shapefiles-brasil/0.01/BR-municipios.geojson"
+@cache_memoize(timeout=None)
+def download_city_geojson(tolerance):
+    url = f"https://data.brasil.io/dataset/shapefiles-brasil/{tolerance}/BR-municipios.geojson"
     data = http_get_json(url, 5)
     return data
 
 
 @cache_memoize(timeout=None)
-def city_geojson():
-    geojson = download_city_geojson()
+def city_geojson(high_fidelity=False):
+    tolerance = 0.01 if not high_fidelity else 0.001
+    geojson = download_city_geojson(tolerance)
     feature_id_func = lambda feature: feature["properties"]["CD_GEOCMU"]
     geojson["features"].sort(key=feature_id_func)
     for feature_id, features in groupby(geojson["features"], key=feature_id_func):
