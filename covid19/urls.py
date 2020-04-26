39dc2fea7e3efd088543509f6dee3d5758cc556a
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-04-25 23:51:59 -0300

    Adiciona rota para estado e / no fim das outras

--- covid19/urls.py
+++ covid19/urls.py
@@ -6,8 +6,9 @@ from . import views
 app_name = "covid19"
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
-    path("voluntarios", views.volunteers, name="volunteers"),
-    path("cities/cases", views.cities, name="cities-cases"),
-    path("cities/geo", views.cities_geojson, name="cities-geo"),
+    path("voluntarios/", views.volunteers, name="volunteers"),
+    path("cities/cases/", views.cities, name="cities-cases"),
+    path("cities/geo/", views.cities_geojson, name="cities-geo"),
     path("import-data/<str:state>/", views.import_spreadsheet_proxy, name="spreadsheet_proxy"),
+    path("<str:state>/", views.dashboard, name="dashboard"),
 ]
