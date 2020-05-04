dc881bb853b855d79b36cc975fe4e0b8bd11c359
Bernardo Fontes <bernardoxhc@gmail.com>
2020-05-04 17:38:17 -0300

    Ignora erro do flake8 por conta de import *

--- brasilio/test_settings.py
+++ brasilio/test_settings.py
@@ -6,4 +6,4 @@ for queue in RQ_QUEUES.values():  # noqa
     queue["ASYNC"] = False
 
 
-SAMPLE_SPREADSHEETS_DATA_DIR = Path(BASE_DIR).joinpath("covid19", "tests", "data")
+SAMPLE_SPREADSHEETS_DATA_DIR = Path(BASE_DIR).joinpath("covid19", "tests", "data")  # noqa
