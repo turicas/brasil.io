4555115fa5fc1c55cb198af5f96c3495c7e7c42b
Bernardo Fontes <bernardoxhc@gmail.com>
2020-05-05 15:42:28 -0300

    Modifica queryset do admin para incluir tabelas escondidas

--- core/admin.py
+++ core/admin.py
@@ -28,7 +28,16 @@ admin.site.register(models.Version, VersionAdmin)
 
 class TableAdmin(MarkdownxModelAdmin):
     def get_queryset(self, request):
-        return super().get_queryset(request).select_related("dataset", "version")
+        """
+        Return a QuerySet of all model instances that can be edited by the
+        admin site. This is used by changelist_view.
+        """
+        qs = models.Table.with_hidden.all()
+        # TODO: this should be handled by some parameter to the ChangeList.
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
 
 
 admin.site.register(models.Table, TableAdmin)
