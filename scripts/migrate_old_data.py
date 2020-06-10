6530d178e092bff81ea98bed00f0c0548b7f77d7
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-06-10 20:16:58 -0300

    Adiciona script que extrai dados antigos e gera JSON

---
+++ scripts/migrate_old_data.py
@@ -0,0 +1,27 @@
+import json
+
+from brazil_data.states import STATES
+from covid19.google_data import get_state_data_from_google_spreadsheets
+
+
+output = "/data/old-data.json"
+data = {}
+for state in STATES:
+    state = state.acronym
+    print(state)
+
+    data[state] = None
+    while data[state] is None:
+        try:
+            data[state] = get_state_data_from_google_spreadsheets(state, timeout=10)
+        except KeyboardInterrupt:
+            break
+        except:
+            print("  error, trying again")
+            continue
+        else:
+            for report in data[state]["reports"]:
+                report["date"] = str(report["date"])
+print("dumping")
+with open(output, mode="w") as fobj:
+    json.dump(data, fobj)
