d7d407a3241e3f8ceb2c06f3928c5c11ae7e727e
Bernardo Fontes <bernardoxhc@gmail.com>
2020-06-23 16:09:57 -0300

    Lista todas as imagens de boletins existentes no banco

--- covid19/views.py
+++ covid19/views.py
@@ -12,7 +12,7 @@ from core.util import cached_http_get_json
 from covid19.epiweek import get_epiweek
 from covid19.exceptions import SpreadsheetValidationErrors
 from covid19.geo import city_geojson, state_geojson
-from covid19.models import StateSpreadsheet
+from covid19.models import StateSpreadsheet, DailyBulletin
 from covid19.spreadsheet import merge_state_data
 from covid19.stats import Covid19Stats, max_values
 
@@ -308,3 +308,9 @@ def status(request):
 
     data.sort(key=row_sort)
     return render(request, "covid-status.html", {"import_data": data})
+
+
+@disable_non_logged_user_cache
+def list_bulletins(request):
+    bulletins = DailyBulletin.objects.order_by('-date')
+    return render(request, "covid-bulletins.html", {'bulletins': bulletins})
