f10fa0161a048e4c02c07159ca9030273b4b6a25
Bernardo Fontes <bernardoxhc@gmail.com>
2020-06-25 12:25:37 -0300

    make lint

--- covid19/management/commands/update_bulletin.py
+++ covid19/management/commands/update_bulletin.py
@@ -1,7 +1,7 @@
-from pathlib import Path
 import datetime
-import rows
+from pathlib import Path
 
+import rows
 from django.core.management.base import BaseCommand
 from django.core.validators import URLValidator
 
@@ -12,20 +12,19 @@ class Command(BaseCommand):
     help = "Update/create daily Covid19 bulletin"
 
     def add_arguments(self, parser):
-        parser.add_argument('date', type=str, help="Date in format YYYY-MM-DD")
-        parser.add_argument('image_url', type=str, help="Bulletin image URL")
+        parser.add_argument("date", type=str, help="Date in format YYYY-MM-DD")
+        parser.add_argument("image_url", type=str, help="Bulletin image URL")
         parser.add_argument("--csv", type=Path, help="CSV file to update the bulletin JSON field")
 
     def clean_args(self, **kwargs):
-        date, image_url, csv_file = kwargs['date'], kwargs['image_url'], kwargs['csv']
-        date = datetime.date(*[int(n) for n in date.split('-')])
+        date, image_url, csv_file = kwargs["date"], kwargs["image_url"], kwargs["csv"]
+        date = datetime.date(*[int(n) for n in date.split("-")])
         url_validator = URLValidator()
         url_validator(image_url)
         if csv_file and not csv_file.exists():
             raise Exception(f"The CSV {csv_file} does not exists")
 
         return date, image_url, csv_file
-
 
     def handle(self, *args, **kwargs):
         date, image_url, csv_file = self.clean_args(**kwargs)
@@ -38,7 +37,7 @@ class Command(BaseCommand):
 
         defaults = {"image_url": image_url}
         if json_data:
-            defaults['detailed_data'] = json_data
+            defaults["detailed_data"] = json_data
         bulletin, _ = DailyBulletin.objects.update_or_create(date=date, defaults=defaults)
 
         print(f"Atualizado com sucesso: {bulletin.admin_url}")
