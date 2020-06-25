f31754006e0eacaca1ffd62187379fd3ddcbc726
Bernardo Fontes <bernardoxhc@gmail.com>
2020-06-25 12:25:17 -0300

    Adiciona parâmetro opicional para arquivo de CSV atualizar os dados do JSONField

--- covid19/management/commands/update_bulletin.py
+++ covid19/management/commands/update_bulletin.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 import datetime
+import rows
 
 from django.core.management.base import BaseCommand
 from django.core.validators import URLValidator
@@ -12,15 +14,31 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('date', type=str, help="Date in format YYYY-MM-DD")
         parser.add_argument('image_url', type=str, help="Bulletin image URL")
+        parser.add_argument("--csv", type=Path, help="CSV file to update the bulletin JSON field")
 
-    def handle(self, *args, **kwargs):
-        date, image_url = kwargs['date'], kwargs['image_url']
+    def clean_args(self, **kwargs):
+        date, image_url, csv_file = kwargs['date'], kwargs['image_url'], kwargs['csv']
         date = datetime.date(*[int(n) for n in date.split('-')])
         url_validator = URLValidator()
         url_validator(image_url)
+        if csv_file and not csv_file.exists():
+            raise Exception(f"The CSV {csv_file} does not exists")
+
+        return date, image_url, csv_file
+
+
+    def handle(self, *args, **kwargs):
+        date, image_url, csv_file = self.clean_args(**kwargs)
+
+        json_data = []
+        if csv_file:
+            json_data = [r._asdict() for r in rows.import_from_csv(csv_file)]
 
         print(f"Atualizando imagem do boletim {date} para {image_url}")
 
-        bulletin, _ = DailyBulletin.objects.update_or_create(date=date, defaults={"image_url": image_url})
+        defaults = {"image_url": image_url}
+        if json_data:
+            defaults['detailed_data'] = json_data
+        bulletin, _ = DailyBulletin.objects.update_or_create(date=date, defaults=defaults)
 
         print(f"Atualizado com sucesso: {bulletin.admin_url}")
