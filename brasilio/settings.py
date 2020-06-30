6d67705d21795f917514693e881b39010f3ed884
Álvaro Justen <alvarojusten@gmail.com>
2020-06-30 19:34:33 -0300

    Apply suggestions from code review

--- brasilio/settings.py
+++ brasilio/settings.py
@@ -11,7 +11,7 @@ env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(".env")
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS", default="").split(",")
-BLOCKED_AGENTS = env("BLOCKED_AGENTS", default="").split(",")
+BLOCKED_AGENTS = env.list("BLOCKED_AGENTS", default=[])
 BASE_DIR = root()
 DEBUG = env("DEBUG")
 PRODUCTION = env("PRODUCTION", bool)
