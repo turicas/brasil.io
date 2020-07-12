c4458bdaaea17a32fb994e3ff7999cf7f7a09eb5
Pedro Belli de Souza Oliveira <pedro.belli2@gmail.com>
2020-07-11 21:02:10 -0300

    Add covid report template.

---
+++ covid19/templates/daily-bulletin/index-relatório.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    const casesBase = document.getElementById("casesBase");
+    const casesMiddle = document.getElementById("casesMiddle");
+    const casesFinal = document.getElementById("casesFinal");
+    const deathsBase = document.getElementById("deathsBase");
+    const deathsMiddle = document.getElementById("deathsMiddle");
+    const deathsFinal = document.getElementById("deathsFinal");
+
+    casesBase.textContent = "0";
+    casesMiddle.textContent = "100 mil";
+    casesFinal.textContent = "200 mil";
+    deathsBase.textContent = "0";
+    deathsMiddle.textContent = "5 mil";
+    deathsFinal.textContent = "10 mil";
+}
