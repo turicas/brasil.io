123b0fbc78e0cc75a0ba7b0219c017c715bb40d3
Marcel Marques <marcmatiasdev@gmail.com>
2020-09-04 16:37:48 -0300

    Donors page endswith template function and link in base page

---
+++ core/templatetags/endswith.py
@@ -0,0 +1,9 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter(name="endswith")
+@stringfilter
+def endswith(value, suffix):
+    return value.endswith(suffix)
