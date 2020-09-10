d296b132b8574c795dc4ee2b4e97d7f37567813f
Bernardo Fontes <bernardoxhc@gmail.com>
2020-09-10 17:04:37 -0300

    Desabilita warning de arquivo .env não existe

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import warnings
 
 if __name__ == "__main__":
+    warnings.filterwarnings('ignore', module='environ')  # disable missing .env warning
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brasilio.settings")
     try:
         from django.core.management import execute_from_command_line
