3041824c8ba8de0b21c2fb65556fb050792a0c37
Bernardo Fontes <bernardoxhc@gmail.com>
2020-09-17 19:35:23 -0300

    Melhora listagem no admin

--- traffic_control/admin.py
+++ traffic_control/admin.py
@@ -5,6 +5,7 @@ from traffic_control.models import BlockedRequest
 
 class BlockedRequestAdmin(admin.ModelAdmin):
     readonly_fields = ["request_data", "created_at"]
+    list_display = ["source_ip", "status_code", "path", "user_agent", "created_at"]
 
 
 admin.site.register(BlockedRequest, BlockedRequestAdmin)
