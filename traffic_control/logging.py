4075f1cb2fb3d37ad883c6b8558afc2ec56df191
Bernardo Fontes <bernardoxhc@gmail.com>
2020-09-17 19:28:30 -0300

    Popula remote addr
    Co-authored-by: Álvaro Justen <alvarojusten@gmail.com>

--- traffic_control/logging.py
+++ traffic_control/logging.py
@@ -15,6 +15,7 @@ def log_blocked_request(request, response_status_code):
         "user_id": getattr(user, "id", None),
         "http": {key: value for key, value in request.META.items() if key.lower().startswith("http_")},
     }
+    request_data["http"]["remote-addr"] = request.META.get("REMOTE_ADDR", "").strip()
     if settings.RQ_BLOCKED_REQUESTS_LIST:
         conn = get_redis_connection("default")
         conn.lpush(settings.RQ_BLOCKED_REQUESTS_LIST, json.dumps(request_data))
