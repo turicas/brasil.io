5c032673e275e992eec0f7bed6d469f659a207be
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-09-17 19:52:46 -0300

    Campos do traffic_control podem ser nulos

--- traffic_control/models.py
+++ traffic_control/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class BlockedRequest(models.Model):
     request_data = models.JSONField(null=False)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
-    user_agent = models.TextField(default="")
-    headers = models.JSONField(default=dict)
-    query_string = models.JSONField(default=dict)
-    path = models.TextField(default="")
-    source_ip = models.GenericIPAddressField(null=True)
-    status_code = models.PositiveSmallIntegerField(default=429)
+    user_agent = models.TextField(default="", null=True, blank=True)
+    headers = models.JSONField(default=dict, null=True, blank=True)
+    query_string = models.JSONField(default=dict, null=True, blank=True)
+    path = models.TextField(default="", null=True, blank=True)
+    source_ip = models.GenericIPAddressField(null=True, blank=True)
+    status_code = models.PositiveSmallIntegerField(default=429, null=True, blank=True)
 
     @classmethod
     def from_request_data(cls, request_data):
