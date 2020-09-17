320a9e88b09c46b13765e44bcb597065a5ac9431
Bernardo Fontes <bernardoxhc@gmail.com>
2020-09-17 18:42:19 -0300

    Cria model muito simples para persistir as requests bloqueads

--- traffic_control/admin.py
+++ traffic_control/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from traffic_control.models import BlockedRequest
+
+
+class BlockedRequestAdmin(admin.ModelAdmin):
+    readonly_fields = ["request_data", "created_at"]
+
+
+admin.site.register(BlockedRequest, BlockedRequestAdmin)
--- traffic_control/models.py
+++ traffic_control/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+
+class BlockedRequest(models.Model):
+    request_data = models.JSONField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ["-created_at"]
