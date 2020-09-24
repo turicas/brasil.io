6a7c5e87401bc4a62c1e74a2e87957805ed6b0d6
Bernardo Fontes <bernardoxhc@gmail.com>
2020-09-23 21:43:52 -0300

    Entrypoint via management commmand do Django

---
+++ traffic_control/management/commands/update_cloudflare_blocked_ips.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from traffic_control.commands import UpdateBlockedIPsCommand
+
+
+class Command(BaseCommand):
+    help = "Update Cloudflare blocked ips"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--hourly-max", required=False, type=int)
+        parser.add_argument("--daily-max", required=False, type=int)
+
+    def handle(self, *args, **kwargs):
+        hourly_max = kwargs["hourly_max"] or 30
+        daily_max = kwargs["daily_max"] or 1200
+
+        UpdateBlockedIPsCommand.execute(
+            settings.CLOUDFLARE_ACCOUNT_NAME,
+            settings.CLOUDFLARE_BLOCKED_IPS_RULE,
+            hourly_max=hourly_max,
+            daily_max=daily_max,
+        )
