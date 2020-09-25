9787d043ceba7ba4b0abf9bb89ade806ad9bad13
Bernardo Fontes <bernardoxhc@gmail.com>
2020-09-24 21:01:54 -0300

    Cria comando para agendar tarefas da app traffic control e o executa no processo de release da app

--- bin/release.sh
+++ bin/release.sh
@@ -5,3 +5,4 @@ set -o pipefail
 set -o nounset
 
 python manage.py migrate --no-input
+python manage.py schedule_traffic_control_jobs
---
+++ traffic_control/management/commands/schedule_traffic_control_jobs.py
@@ -0,0 +1,19 @@
+import django_rq
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from traffic_control import tasks
+
+
+class Command(BaseCommand):
+    help = "Schedule recurrent traffic control jobs"
+
+    def schedule(self, func, interval):
+        scheduler = django_rq.get_scheduler("default")
+        job = scheduler.schedule(
+            scheduled_time=timezone.now(), func=tasks.persist_blocked_requests_task, interval=interval, repeat=None,
+        )
+        print(f"Task {func.__name__} scheduled as {job}")
+
+    def handle(self, *args, **kwargs):
+        self.schedule(tasks.persist_blocked_requests_task, 300)
