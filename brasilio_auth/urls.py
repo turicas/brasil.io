f036ab93850ebd13ac52cd8dee1f1f9a885e4b96
Bernardo Fontes <bernardoxhc@gmail.com>
2020-09-28 16:59:43 -0300

    Adiciona rotas para fluxo de autenticação de 2 passos

--- brasilio_auth/urls.py
+++ brasilio_auth/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, reverse_lazy
+from django.views.generic.base import TemplateView
 
 from brasilio_auth.views import CreateUserView
 from core.middlewares import disable_non_logged_user_cache
+import django_registration.backends.activation.views as registration_views
 
 login = auth_views.LoginView.as_view(template_name="brasilio_auth/login.html")
 logout = auth_views.LogoutView.as_view()
@@ -25,10 +27,36 @@ password_reset_complete = auth_views.PasswordResetCompleteView.as_view(
 app_name = "brasilio_auth"
 urlpatterns = (
     path("login/", login, name="login"),
-    path("entrar/", disable_non_logged_user_cache(CreateUserView.as_view()), name="sign_up"),
     path("logout/", logout, name="logout"),
     path("troca-senha/", password_reset, name="password_reset"),
     path("troca-senha/enviada/", password_reset_done, name="password_reset_done"),
     path("troca-senha/<uidb64>/<token>/", password_reset_confirm, name="password_reset_confirm",),
     path("troca-senha/atualizada/", password_reset_complete, name="password_reset_complete",),
+    path(
+        "ativar/sucesso/",
+        TemplateView.as_view(
+            template_name="brasilio_auth/activation_complete.html"
+        ),
+        name="activation_complete",
+    ),
+    path(
+        "ativar/<str:activation_key>/",
+        registration_views.ActivationView.as_view(),
+        name="activate_user",
+    ),
+    path("entrar/", disable_non_logged_user_cache(CreateUserView.as_view()), name="sign_up"),
+    path(
+        "entrar/sucesso/",
+        TemplateView.as_view(
+            template_name="brasilio_auth/registration_complete.html"
+        ),
+        name="sign_up_complete",
+    ),
+    path(
+        "entrar/fechado/",
+        TemplateView.as_view(
+            template_name="brasilio_auth/registration_closed.html"
+        ),
+        name="sign_up_disallowed",
+    ),
 )
