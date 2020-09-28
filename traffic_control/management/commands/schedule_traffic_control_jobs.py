ba82ee0e4b13f0abdc247f236240e0d13bbb4c66
Bernardo Fontes <bernardoxhc@gmail.com>
2020-09-28 16:14:26 -0300

    Corrige referência hardcoded e deixa de acumular tarefas agendadas

--- traffic_control/management/commands/schedule_traffic_control_jobs.py
+++ traffic_control/management/commands/schedule_traffic_control_jobs.py
@@ -1,4 +1,5 @@
 import django_rq
+from cached_property import cached_property
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
@@ -8,13 +9,17 @@ from traffic_control import tasks
 class Command(BaseCommand):
     help = "Schedule recurrent traffic control jobs"
 
+    @cached_property
+    def scheduler(self):
+        return django_rq.get_scheduler("default")
+
     def schedule(self, func, interval):
-        scheduler = django_rq.get_scheduler("default")
-        job = scheduler.schedule(
-            scheduled_time=timezone.now(), func=tasks.persist_blocked_requests_task, interval=interval, repeat=None,
-        )
+        job = self.scheduler.schedule(scheduled_time=timezone.now(), func=func, interval=interval, repeat=None,)
         print(f"Task {func.__name__} scheduled as {job}")
 
     def handle(self, *args, **kwargs):
+        for job in self.scheduler.get_jobs():
+            self.scheduler.cancel(job)
+
         self.schedule(tasks.persist_blocked_requests_task, 300)
         self.schedule(tasks.update_blocked_ips_task, 3600)
