cc7f4d2488646f2638e88bd49ff5f4ccb90261b7
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-01 15:36:25 -0300

    Refatora código para estruturar atividades como dataclass

--- data_activities_log/activites.py
+++ data_activities_log/activites.py
@@ -1,18 +1,25 @@
 import datetime
 import itertools
+from dataclasses import dataclass
 
 from django.utils import timezone
 
 from core.models import Table
 
 
-def recent_activities(days_ago=30, limit=None):
+@dataclass
+class Activity:
+    title: str
+    url: str
+    created_at: datetime
+
+
+def dataset_updates(days_ago):
     days_ago = timezone.now() - datetime.timedelta(days=30)
     tables_recently_updated = (
         Table.objects.filter(import_date__gte=days_ago).order_by("-import_date").select_related("dataset")
     )
 
-    activities = []
     datasets_with_updates = []
     for metadata, tables in itertools.groupby(
         tables_recently_updated, key=lambda t: (t.dataset.slug, t.import_date.date())
@@ -20,15 +27,20 @@ def recent_activities(days_ago=30, limit=None):
         dataset_slug, date = metadata
         if dataset_slug in datasets_with_updates:
             continue
-        tables = list(tables)
-        name = tables[0].dataset.name
-        plural = len(tables) > 1
-        activities.append(
-            {
-                "date": date,
-                "description": f"Tabela{'s' if plural else ''} {', '.join(t.name for t in tables)} atualizadas no dataset {name}",
-            }
-        )
         datasets_with_updates.append(dataset_slug)
 
+        tables = list(tables)
+        dataset = tables[0].dataset
+        plural = len(tables) > 1
+        desc = f"Tabela{'s' if plural else ''} {', '.join(t.name for t in tables)} atualizadas no dataset {dataset.name}"
+
+        yield Activity(
+            title=desc,
+            url="",
+            created_at=max([t.import_date for t in tables]),
+        )
+
+
+def recent_activities(days_ago=30, limit=None):
+    activities = list(dataset_updates(days_ago))
     return activities[:limit]
