d831d25c6e0333f7088d791e78524c0a10bebbde
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-05 17:21:12 -0300

    Make lint

--- brasilio_auth/views.py
+++ brasilio_auth/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 
 from api.models import NumMaxTokensExceeded, Token
