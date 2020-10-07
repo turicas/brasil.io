96315fc917d11a2aa536e34c096e2c94e4b0cfc1
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-07 15:08:30 -0300

    Fix typo

--- brasilio/test_settings.py
+++ brasilio/test_settings.py
@@ -10,5 +10,5 @@ CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache",}
 RQ_BLOCKED_REQUESTS_LIST = ""
 
 RATELIMIT_ENABLE = False  # noqa
-TEMPLATE_STRING_IF_INVALID = "%%%Invalid varialbe%%%"  # noqa
+TEMPLATE_STRING_IF_INVALID = "%%%Invalid variable%%%"  # noqa
 TEMPLATES[0]["OPTIONS"]["string_if_invalid"] = TEMPLATE_STRING_IF_INVALID  # noqa
