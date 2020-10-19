4a295ad158a0521d6c9cd2fb6c138b8f998ef2e3
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-19 17:20:02 -0300

    Garante que requisições da API estão sendo processadas pelos handlers esperados

--- brasilio/api_urls.py
+++ brasilio/api_urls.py
@@ -4,3 +4,5 @@ urlpatterns = [
     path("", include("api.urls_v0", namespace="api-v0")),
     path("v1/", include("api.urls", namespace="api-v1")),
 ]
+
+handler403 = "traffic_control.handlers.handler_403"
