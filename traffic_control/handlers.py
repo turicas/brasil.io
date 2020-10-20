b4aa2239eb802439f9f7d7ba2cdc8474851b105f
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-20 16:38:46 -0300

    Lint

--- traffic_control/handlers.py
+++ traffic_control/handlers.py
@@ -1,8 +1,3 @@
-import base64
-import os
-import random
-from functools import lru_cache
-
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
