fe9c3531d714d24b3249e848845b6685c5ec7bb2
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-30 15:05:14 -0300

    Redirects de versão da API considerando os parâmetros de querystring

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,3 +1,19 @@
+from urllib.parse import urlencode
+
+from django.urls import resolve, reverse
+
+
 class ApiEndpointFromOldVersionException(Exception):
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, request):
+        self.request = request
+
+    @property
+    def should_redirect_to(self):
+        match = resolve(self.request.path)
+        url = reverse(f"api-v1:{match.url_name}", args=match.args, kwargs=match.kwargs)
+
+        qs = urlencode(self.request.query_params)
+        if qs:
+            url += f"?{qs}"
+
+        return url
