793e77b3576c45f6499a9fb35d28c0db34df1c63
Bernardo Fontes <bernardoxhc@gmail.com>
2020-11-27 16:19:34 -0300

    Recupera host de api do settings

--- api/docs/views.py
+++ api/docs/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render
 
 from core.models import Dataset
@@ -6,5 +7,6 @@ from core.models import Dataset
 def dynamic_api_docs_view(request):
     context = {
         "datasets": Dataset.objects.api_visible(),
+        "api_host": settings.BRASILIO_API_HOST,
     }
     return render(request, "api/docs.html", context=context)
