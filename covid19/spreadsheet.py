4ca6a3fccd778aeb1e0b9f29eb4ef653394d4777
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2021-02-01 01:03:11 -0300

    Conserta datas (adiciona ano, alterando formato antigo)

--- covid19/spreadsheet.py
+++ covid19/spreadsheet.py
@@ -5,10 +5,34 @@ from covid19.models import StateSpreadsheet
 from covid19.spreadsheet_validator import TOTAL_LINE_DISPLAY, UNDEFINED_DISPLAY
 
 
+def fix_key(key):
+    """
+    >>> fix_key("municipio")
+    'municipio'
+    >>> fix_key("confirmados_17_03")
+    'confirmados_2020-03-17'
+    >>> fix_key("mortes_02_03")
+    'mortes_2020-03-02'
+    """
+    if key == "municipio":
+        return key
+
+    label, day, month = key.split("_")
+    return f"{label}_2020-{month}-{day}"
+
+
 def merge_state_data(state):
     gs_data = google_data.get_base_data()[state]  # Get data from old Google Spreadsheets
     original_reports = gs_data["reports"]
-    original_cases = gs_data["cases"]
+    # Fix format of old data from Google Spreadsheets (add year to date)
+    original_cases = [
+        {
+            fix_key(key): value
+            for key, value in city_cases.items()
+            if not key.startswith("field_")
+        }
+        for city_cases in gs_data["cases"]
+    ]
 
     db_data = StateSpreadsheet.objects.get_state_data(state)  # Get data from database
     new_reports = db_data["reports"]
@@ -38,8 +62,7 @@ def merge_state_data(state):
 
         original_cities.add(city)
         for date, values_for_date in new_cases.items():
-            date_str = f"{date.day:02d}_{date.month:02d}"
-            # TODO: date problem here
+            date_str = date.isoformat()  # YYYY-MM-DD
             city_on_date = values_for_date.get(city, {})
             row[f"confirmados_{date_str}"] = city_on_date.get("confirmed", None)
             row[f"mortes_{date_str}"] = city_on_date.get("deaths", None)
@@ -48,8 +71,7 @@ def merge_state_data(state):
     # recent IBGE data can add new cities that weren't present in the original data
     new_rows = {}
     for date, values_for_date in new_cases.items():
-        date_str = f"{date.day:02d}_{date.month:02d}"
-        # TODO: date problem here
+        date_str = date.isoformat()  # YYYY-MM-DD
         for city, data in values_for_date.items():
             city_info = get_city_info(city, state)
             if city_info:
@@ -80,18 +102,17 @@ def merge_state_data(state):
 
 
 def row_with_sorted_columns(row):
-    row_dates = set()
-    for key in row.keys():
-        if not key.startswith("confirmados"):
-            continue
-        label, day, month = key.split("_")
-        row_dates.add(f"2020-{int(month):02d}-{int(day):02d}")
+    row_dates = set(
+        key.split("_")[1]
+        for key in row.keys()
+        if key.startswith("confirmados_")
+    )
 
     new = {"municipio": row["municipio"]}
     for date_str in sorted(row_dates, reverse=True):
         year, month, day = date_str.split("-")
-        confirmed = f"confirmados_{day}_{month}"
-        deaths = f"mortes_{day}_{month}"
+        confirmed = f"confirmados_{date_str}"
+        deaths = f"mortes_{date_str}"
         new[confirmed] = row[confirmed]
         new[deaths] = row[deaths]
 
