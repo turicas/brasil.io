5dd95f15641e6a0e413352b3d164922e797ff9a3
Marcel Marques <marcmatiasdev@gmail.com>
2021-04-05 09:10:10 -0300

    Subindo app clipping para gerenciamento de links e referencias a datasets do brasilio

---
+++ clipping/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('get/contentype_instances/', views.get_contenttype_instances),
+    path('get/selected_instance/', views.get_current_selected_instance),
+]
