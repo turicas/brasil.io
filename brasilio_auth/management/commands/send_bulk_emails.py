39e92eed059969655317b2ece1f89fe2bfa40b8d
Rhenan Bartels <rhenan.bartels@gmail.com>
2021-04-10 13:25:40 -0300

    Aprimora script de envio de e-mails

--- brasilio_auth/management/commands/send_bulk_emails.py
+++ brasilio_auth/management/commands/send_bulk_emails.py
@@ -1,5 +1,6 @@
 import django_rq
 import rows
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.template import Context, Template
 
@@ -14,12 +15,17 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("input_filename")
         parser.add_argument("email_template")
+        parser.add_argument("--from")
         parser.add_argument("-d", "--dry-run", default=False, action="store_true")
 
     def handle(self, *args, **kwargs):
         input_filename = kwargs["input_filename"]
         table = rows.import_from_csv(input_filename)
+        error_msg = "Arquivo CSV deve conter campos 'to_email' e 'subject'"
+        assert {"to_email", "subject"}.issubset(set(table.field_names)), error_msg
+
         template_obj = self.load_email_template(kwargs["email_template"])
+        from_email = kwargs["from"] or settings.DEFAULT_FROM_EMAIL
 
         for row in table:
             context = Context(row._asdict())
@@ -27,10 +33,10 @@ class Command(BaseCommand):
             if not kwargs["dry_run"]:
                 django_rq.enqueue(
                     send_email,
-                    subject="Subject",
+                    subject=row.subject,
                     body=rendered_template,
-                    from_email="from@example.com",
-                    to=["to@example.com"],
+                    from_email=from_email,
+                    to=[row.to_email],
                 )
             else:
                 print(rendered_template)
--- brasilio_auth/tests/test_commands.py
+++ brasilio_auth/tests/test_commands.py
@@ -1,5 +1,7 @@
 from tempfile import NamedTemporaryFile
 
+import pytest
+from django.conf import settings
 from django.core import mail
 from django.core.management import call_command
 from django.test import TestCase
@@ -11,7 +13,10 @@ class TestSendBulkEmails(TestCase):
         self.email_template = NamedTemporaryFile(suffix=".txt")
 
         with open(self.input_file.name, "w") as fobj:
-            fobj.write("nome,data\nnome_1,data_1\nnome_2,data_2")
+            fobj.write(
+                "nome,data,to_email,subject\nnome_1,data_1,email_1,subject_1\n"
+                "nome_2,data_2,email_2,subject_2"
+            )
 
         with open(self.email_template.name, "w") as fobj:
             fobj.write("Enviado para {{ nome }} em {{ data }}")
@@ -22,9 +27,15 @@ class TestSendBulkEmails(TestCase):
         self.expexted_send_email = [
             {
                 "body": "Enviado para nome_1 em data_1",
+                "subject": "subject_1",
+                "to": ["email_1"],
+                "from_email": settings.DEFAULT_FROM_EMAIL,
             },
             {
                 "body": "Enviado para nome_2 em data_2",
+                "subject": "subject_2",
+                "to": ["email_2"],
+                "from_email": settings.DEFAULT_FROM_EMAIL,
             }
         ]
 
@@ -38,6 +49,30 @@ class TestSendBulkEmails(TestCase):
         assert len(mail.outbox) == 2
         self.assert_sent_email_metadata(mail.outbox[0], self.expexted_send_email[0])
         self.assert_sent_email_metadata(mail.outbox[1], self.expexted_send_email[1])
+
+    def test_send_email_custom_from_email(self):
+        kwargs = {"from": "Example Email <email@example.com>"}
+        call_command(
+            "send_bulk_emails",
+            self.input_file.name,
+            self.email_template.name,
+            **kwargs
+        )
+
+        self.expexted_send_email[0]["from_email"] = kwargs["from"]
+        self.expexted_send_email[1]["from_email"] = kwargs["from"]
+
+        assert len(mail.outbox) == 2
+        self.assert_sent_email_metadata(mail.outbox[0], self.expexted_send_email[0])
+        self.assert_sent_email_metadata(mail.outbox[1], self.expexted_send_email[1])
+
+    def test_assert_mandatory_fields(self):
+        with open(self.input_file.name, "w") as fobj:
+            fobj.write("nome,data\nnome_1,data_1\nnome_2,data_2")
+
+        self.input_file.seek(0)
+        with pytest.raises(AssertionError):
+            call_command("send_bulk_emails", self.input_file.name, self.email_template.name)
 
     def test_do_not_send_mail(self):
         call_command(
