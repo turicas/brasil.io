7a907d8cd389a193d824b520d7ad0ce7382d65a3
Marcel Marques <marcmatiasdev@gmail.com>
2022-01-31 21:57:42 -0300

    Forms and alerts updates and enhancements
    * UserCreationForm
    * ClippingForms
    * Table Dynamic Forms, datasets filter bootstrap forms enhancements
    using vanillajs calendars in date types

--- brasilio_auth/forms.py
+++ brasilio_auth/forms.py
@@ -18,17 +18,47 @@ def is_valid_username(username):
 
 class UserCreationForm(RegistrationFormUniqueEmail):
     username = forms.CharField(
-        widget=forms.TextInput(),
+        widget=forms.TextInput(
+            attrs={
+                'type':'text',
+                'class':"form-control"
+            }
+        ),
+        label=_("Usuário"),
+        required=True
     )
-    email = forms.EmailField()
-    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    email = forms.EmailField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'ex@mail.com',
+                'type':'email',
+                'class':"form-control"
+            }
+        ),
+        required=True
+    )
+    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput(
+        attrs={
+            'class':"form-control"
+        }
+    ))
     password2 = forms.CharField(
         label=_("Password confirmation"),
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(
+        attrs={
+            'class':"form-control"
+        }),
         help_text=_("Enter the same password as above, for verification."),
     )
     captcha = ReCaptchaField(required=True)
-    subscribe_newsletter = forms.BooleanField(required=False)
+    subscribe_newsletter = forms.BooleanField(widget=forms.CheckboxInput(
+            attrs={
+                'type':'checkbox',
+                'class':'form-check-input'
+            },
+        ),
+        required=False
+    )
 
     class Meta:
         model = get_user_model()
