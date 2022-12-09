a5d8c36ee1e9aeb5fcb62d2865be7a6ecca67fda
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-12-09 20:37:17 -0300

    Autoreload gunicorn on dev env

--- bin/web.sh
+++ bin/web.sh
@@ -7,13 +7,13 @@ set -o nounset
 if [ -z ${PORT+x} ]; then
 	PORT=5000
 fi
-if [ -z ${WORKERS+x} ]; then
-	WORKERS=4
+if [ -z ${WEB_WORKERS+x} ]; then
+	WEB_WORKERS=4
+fi
+OPTS="--chdir=/app --bind=0.0.0.0:$PORT --workers=$WEB_WORKERS --log-file -"
+if [[ $(echo $DEBUG | tr a-z A-Z) = "TRUE" ]]; then
+	OPTS="$OPTS --reload"
 fi
 
 python manage.py collectstatic --noinput
-gunicorn brasilio.wsgi:application \
-	--bind="0.0.0.0:$PORT" \
-	--chdir=/app \
-	--workers=$WORKERS \
-	--log-file -
+gunicorn brasilio.wsgi:application $OPTS
