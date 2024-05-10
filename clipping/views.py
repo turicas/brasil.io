56fa90da1efd7d1fec2c8cdbeeb9855687d14927
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2024-05-10 18:36:50 -0300

    make lint

--- clipping/views.py
+++ clipping/views.py
@@ -1,17 +1,18 @@
 import json
-from django.http import HttpResponse
-from django.shortcuts import render  # get_object_or_404, redirect
+
 from django.contrib.contenttypes.models import ContentType
-from .models import Clipping, ClippingRelation
+from django.http import HttpResponse
+
+from .models import ClippingRelation
 
 
 def get_contenttype_instances(request):
-    pk = request.GET.get('id')
-    result = list(ContentType.objects.get(id=pk).model_class().objects.filter().values('id', 'name'))
+    pk = request.GET.get("id")
+    result = list(ContentType.objects.get(id=pk).model_class().objects.filter().values("id", "name"))
     return HttpResponse(json.dumps(result), content_type="application/json")
 
 
 def get_current_selected_instance(request):
-    pk = request.GET.get('id')
+    pk = request.GET.get("id")
     result = ClippingRelation.objects.get(id=pk).object_id
     return HttpResponse(json.dumps(result), content_type="application/json")
