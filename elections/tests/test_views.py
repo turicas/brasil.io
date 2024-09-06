1a6af515917480799d46fa9a5f57e2f2c732d872
rhenanbartels <rhenan.bartels@gmail.com>
2024-09-06 11:47:02 -0300

    Remove elections api tests

--- elections/tests/test_views.py
+++ elections/tests/test_views.py
@@ -7,40 +7,6 @@ from elections.models import Candidacy, CandidacyMetadata
 
 
 @pytest.mark.django_db
-class TestDetailCandidacy:
-    url_name = "elections:api_candidacy_detail"
-    http_user_agent = "test"
-
-    def test_get_detail_candidacy(self, client, settings):
-        candidacy = baker.make(Candidacy, data_nascimento="1981-01-12", _fill_optional=True)
-
-        url = reverse(self.url_name, kwargs={"pk": candidacy.pk})
-        resp = client.get(url, HTTP_USER_AGENT="test-user-agent")
-        expected_data = {"data_nascimento": "12/01/1981"}
-
-        assert resp.status_code == 200
-        assert resp.json()["data_nascimento"] == expected_data["data_nascimento"]
-
-    def test_get_detail_candidacy_wrong_data_nascimento_format(self, client, settings):
-        candidacy = baker.make(Candidacy, data_nascimento="1981-01", _fill_optional=True)
-
-        url = reverse(self.url_name, kwargs={"pk": candidacy.pk})
-        resp = client.get(url, HTTP_USER_AGENT="test-user-agent")
-        expected_data = {"data_nascimento": None}
-
-        assert resp.status_code == 200
-        assert resp.json()["data_nascimento"] == expected_data["data_nascimento"]
-
-    def test_get_detail_candidacy_not_found(self, client, settings):
-        candidacy = baker.make(Candidacy)
-
-        url = reverse(self.url_name, kwargs={"pk": candidacy.pk + 1})
-        resp = client.get(url, HTTP_USER_AGENT="test-user-agent")
-
-        assert resp.status_code == 404
-
-
-@pytest.mark.django_db
 class TestDetailCandidacyView:
     url_name = "elections:candidacy_detail"
     http_user_agent = "test"
