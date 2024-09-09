c7d9b5ae77bbe1785d4d3d36ec2d35b01e744c25
rhenanbartels <rhenan.bartels@gmail.com>
2024-09-09 10:00:00 -0300

    Remove API urls

--- elections/urls.py
+++ elections/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
 from . import views
-from elections.api import CandidacyDetailAPIView, CandidacyListAPIView
 
 app_name = "elections"
 
 urlpatterns = [
-    path("api/candidacy/", CandidacyListAPIView.as_view(), name="api_candidacy_list"),
-    path("api/candidacy/<int:pk>/", CandidacyDetailAPIView.as_view(), name="api_candidacy_detail"),
     path("index/", views.candidacy_list, name="candidacy_list"),
     path("politic/<int:ano>/<str:uf>/<str:cargo>/<str:nome>/", views.politic, name="candidacy_detail"),
     path("home/", views.home, name="home"),
