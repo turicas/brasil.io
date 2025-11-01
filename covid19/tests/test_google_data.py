26aefc7eab81fe9aac44bd6613380a47615ca7ed
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-11-01 16:19:09 -0300

    Remove teste de retry
    Estava deixando a execução dos testes mais lenta em 10s

--- covid19/tests/test_google_data.py
+++ covid19/tests/test_google_data.py
@@ -5,7 +5,7 @@ import pytest
 from django.core.cache import cache
 from django.test import TestCase
 
-from covid19 import google_data
+import covid19
 
 
 class TestGoogleDataIntegration(TestCase):
@@ -14,23 +14,15 @@ class TestGoogleDataIntegration(TestCase):
         cache.clear()
         assert not cache.keys("*")
 
-        data = google_data.get_general_spreadsheet(timeout=10)
+        data = covid19.google_data.get_general_spreadsheet(timeout=10)
         cache_key = cache.keys("*")[0]
 
         assert data
         assert data == cache.get(cache_key)
 
-    def test_import_info_by_state_exposes_attr_api(self):
-        rj_data = google_data.import_info_by_state("RJ")
+    @patch("covid19.google_data.get_general_spreadsheet")
+    def test_import_info_by_state_exposes_attr_api(self, mock_import):
+        mock_import.return_value = {"SP": {"uf": "SP"}, "RJ": {"uf": "RJ"}, "PR": {"uf": "PR"}}
+        rj_data = covid19.google_data.import_info_by_state("RJ")
 
         assert rj_data.uf == "RJ"
-
-    @patch("covid19.google_data.import_info_by_state")
-    def test_retry_to_fetch_google_spreadsheets_if_any_error(self, mock_import):
-        mock_import.side_effect = Exception()
-
-        with pytest.raises(Exception):
-            google_data.get_state_data_from_google_spreadsheets("RJ")
-
-        mock_import.assert_called_with("RJ")
-        assert 3 == mock_import.call_count
