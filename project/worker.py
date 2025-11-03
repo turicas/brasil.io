230192e77e2d75102376d5803ae4005e61ae12d3
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-11-03 13:57:56 -0300

    Evita erro de importação no `make test`

--- project/worker.py
+++ project/worker.py
@@ -1,12 +1,12 @@
-from raven import Client
-from raven.transport import HTTPTransport
 from rq import Worker
-from rq.contrib.sentry import register_sentry
 
 
 class SentryAwareWorker(Worker):
     def __init__(self, *args, **kwargs):
         from django.conf import settings
+        from raven import Client
+        from raven.transport import HTTPTransport
+        from rq.contrib.sentry import register_sentry
 
         super().__init__(*args, **kwargs)
         client = Client(settings.SENTRY_DSN, transport=HTTPTransport)
