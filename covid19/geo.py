ae59785272ba8632a5d1595c680018675e86c9d2
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-12-25 12:26:50 -0300

    Altera URL base do dataset de GeoJSONs

--- covid19/geo.py
+++ covid19/geo.py
@@ -7,14 +7,14 @@ from core.util import http_get_json
 
 @cache_memoize(timeout=None)
 def download_state_geojson(tolerance):
-    url = f"https://data.brasil.io/dataset/shapefiles-brasil/{tolerance}/BR-UF.geojson"
+    url = f"https://data.brasil.io/dataset/shapefiles/{tolerance}/BR-UF.geojson"
     data = http_get_json(url, 5)
     return data
 
 
 @cache_memoize(timeout=None)
 def download_city_geojson(tolerance):
-    url = f"https://data.brasil.io/dataset/shapefiles-brasil/{tolerance}/BR-municipios.geojson"
+    url = f"https://data.brasil.io/dataset/shapefiles/{tolerance}/BR-municipios.geojson"
     data = http_get_json(url, 5)
     return data
 
