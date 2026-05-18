873ffba3d8064d7a59f195879ab65613dc4e80f2
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2026-05-17 21:48:38 -0300

    Melhora gestão de tokens no Django Admin

--- api/admin.py
+++ api/admin.py
@@ -3,11 +3,36 @@ from django.contrib import admin
 from api.models import Token
 
 
+@admin.register(Token)
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ("key", "user", "created")
-    fields = ("user",)
+    list_display = ("key_obfuscated", "user_username", "user_email", "created")
+    list_filter = (("created", admin.DateFieldListFilter),)
+    list_select_related = ("user",)
+    search_fields = ("key", "user__username", "user__email", "user__first_name", "user__last_name")
+    autocomplete_fields = ("user",)
     ordering = ("-created",)
-    actions = None  # Actions not compatible with mapped IDs.
 
+    @admin.display(description="Key")
+    def key_obfuscated(self, obj):
+        return f"{obj.key[:5]}...{obj.key[-5:]}" if obj.key else ""
 
-admin.site.register(Token, TokenAdmin)
+    @admin.display(description="Login", ordering="user__username")
+    def user_username(self, obj):
+        return obj.user.username
+
+    @admin.display(description="E-mail", ordering="user__email")
+    def user_email(self, obj):
+        return obj.user.email
+
+    def get_fields(self, request, obj=None):
+        if obj is None:
+            return ("user",)
+        return ("user", "key_obfuscated", "created")
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None:
+            return ()
+        return ("user", "key_obfuscated", "created")
+
+    def has_change_permission(self, request, obj=None):
+        return False
